68667303a6e6c6eae6f2b3b20ab1ef3d506cac48
Cosmic0wl <valentinaspanetta@gmail.com>
2019-06-14 15:12:46 +0200

    personalised + getcard func

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,4 @@
-// Parent Class
+// Set Content function
 var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
         extendStatics = Object.setPrototypeOf ||
@@ -12,6 +12,8 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+// document.addEventListener("DOMContentLoaded")
+// Parent Class
 var Place = /** @class */ (function () {
     function Place(image, name, city, address, zipCode, date) {
         this.image = "";
@@ -19,7 +21,7 @@ var Place = /** @class */ (function () {
         this.city = "";
         this.address = "";
         this.zipCode = "";
-        this.date = "";
+        this.date = new Date();
         this.image = image;
         this.name = name;
         this.city = city;
@@ -27,10 +29,13 @@ var Place = /** @class */ (function () {
         this.zipCode = zipCode;
         this.date = date;
     }
+    //get card function
+    Place.prototype.getCard = function () {
+        return "<div class=\"col-lg-3 col-md-6 col-sm-12\">\n\t\t\t\t\n\t\t\t\t<div class=\"card\">\n  \t\t\t\t<img class=\"card-img-top\" src=" + this.image + " alt=\"location\">\n  \t\t\t\t<div class=\"card-body\">\n    \t\t\t<h5 class=\"card-title\">" + this.name + "</h5>\n    \t\t\t<p class=\"card-text\">" + this.address + " " + this.zipCode + "</p>\n    \t\t\t<p class=\"card-text\">" + this.city + "</p>\n    \t\t\t<p>" + this.date.toDateString() + "</p>\n  \t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t\t</div>";
+    };
     // render function
     Place.prototype.display = function () {
-        return document.getElementById("places").innerHTML +=
-            "<div class=\"col-lg-3 col-md-6 col-sd-12\">\n\t\t\t\t<div class=\"card\">\n  \t\t\t\t<img class=\"card-img-top\" src=" + this.image + " alt=\"location\">\n  \t\t\t\t<div class=\"card-body\">\n    \t\t\t<h5 class=\"card-title\">" + this.name + "</h5>\n    \t\t\t<p class=\"card-text\"> " + this.address + " " + this.zipCode + " " + this.city + "</p>\n    \t\t\t<p>" + this.date + "</p>\n  \t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t\t</div>";
+        document.getElementById("places").innerHTML += this.getCard();
     };
     return Place;
 }());
@@ -44,39 +49,50 @@ var Restaurant = /** @class */ (function (_super) {
         _this.website = website;
         return _this;
     }
-    // render function
+    //get card function
+    Restaurant.prototype.getCard = function () {
+        return "<div class=\"col-lg-3 col-md-6 col-sm-12\">\n\t\t<div class=\"card\">\n  \t\t\t\t<img class=\"card-img-top\" src=" + this.image + " alt=\"location\">\n  \t\t\t\t<div class=\"card-body\">\n    \t\t\t<h5 class=\"card-title\">" + this.name + "</h5>\n    \t\t\t<p class=\"card-text\">" + this.address + " " + this.zipCode + "</p>\n    \t\t\t<p class=\"card-text\">" + this.city + "</p>\n    \t\t\t<p class=\"card-text\">" + this.category + "</p>\n\t\t\t\t<p class=\"card-text\">" + this.telephone + "</p>\n\t\t\t\t<p class=\"card-text\">" + this.website + "</p>\n    \t\t\t<p class=\"card-text\">" + this.date.toDateString() + "</p>\n  \t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t\t</div>";
+    };
+    //render function
     Restaurant.prototype.display = function () {
-        return document.getElementById("restaurants").innerHTML +=
-            _super.prototype.display.call(this) + "\n\t\t<p>" + this.category + "</p>\n\t\t<p>" + this.telephone + "</p>\n\t\t<p>" + this.website + "</p>";
+        document.getElementById("restaurants").innerHTML += this.getCard();
     };
     return Restaurant;
 }(Place));
-var Events = /** @class */ (function (_super) {
-    __extends(Events, _super);
-    function Events(image, name, city, address, zipCode, date, eventDate, eventTime, ticketPrice) {
+var Concert = /** @class */ (function (_super) {
+    __extends(Concert, _super);
+    function Concert(image, name, city, address, zipCode, date, eventDate, eventTime, ticketPrice) {
         var _this = _super.call(this, image, name, city, address, zipCode, date) || this;
         _this.eventDate = eventDate;
         _this.eventTime = eventTime;
         _this.ticketPrice = ticketPrice;
         return _this;
     }
-    // render function
-    Events.prototype.display = function () {
-        return document.getElementById("events").innerHTML +=
-            _super.prototype.display.call(this) + "\n\t\t<p>" + this.eventDate + ", " + this.eventTime + "</p>\n\t\t<p>" + this.ticketPrice + "</p>";
+    //get card function
+    Concert.prototype.getCard = function () {
+        return "<div class=\"col-lg-3 col-md-6 col-sm-12\">\n\t\t<div class=\"card\">\n  \t\t\t\t<img class=\"card-img-top\" src=" + this.image + " alt=\"location\">\n  \t\t\t\t<div class=\"card-body\">\n    \t\t\t<h5 class=\"card-title\">" + this.name + "</h5>\n    \t\t\t<p class=\"card-text\">" + this.address + " " + this.zipCode + "</p>\n    \t\t\t<p class=\"card-text\">" + this.city + "</p>\n    \t\t\t<p class=\"card-text\">" + this.eventDate + ", " + this.eventTime + "</p>\n\t\t\t\t<p class=\"card-text\">" + this.ticketPrice + "</p>\n    \t\t\t<p class=\"card-text\">" + this.date.toDateString() + "</p>\n  \t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t\t</div>";
     };
-    return Events;
+    // render function
+    Concert.prototype.display = function () {
+        document.getElementById("events").innerHTML += this.getCard();
+    };
+    return Concert;
 }(Place));
 // Instances
 var locations = [];
 // places
-locations.push(new Place("resources/img/karlsplatz.jpg", "Karlsplatz", "Vienna", "Karlsplatz 1", "1010", "26 Feb 2019"));
-locations.push(new Place("resources/img/tiergarten.jpg", "Tiergarten", "Vienna", "Maxingstraße 13b", "1130", "29 Feb 2019"));
-locations.push(new Place("resources/img/karlsplatz.jpg", "Karlsplatz", "Vienna", "Karlsplatz 1", "1010", "26 Feb 2019"));
-locations.push(new Place("resources/img/tiergarten.jpg", "Tiergarten", "Vienna", "Maxingstraße 13b", "1130", "29 Feb 2019"));
+locations.push(new Place("resources/img/cliffs.jpg", "Cliffs of Moher", "Linnascor", "County Clare", "037111", new Date(2018, 11, 24)));
+locations.push(new Place("resources/img/skansen.jpg", "Skansen", "Stockholm", "Djurgårdsslätten 51", "11521", new Date(2017, 11, 24)));
+locations.push(new Place("resources/img/highgate.jpg", "Highgate Cemetery", "London", "Swain's Lane", "N66PJ", new Date(2019, 11, 24)));
+locations.push(new Place("resources/img/holyrood.jpg", "Holyrood Abbey", "Edinburgh", "Canongate 1", "8DX", new Date(2018, 1, 24)));
 // restaurants
-locations.push(new Restaurant("resources/img/lemonleaf.png", "Lemon Leaf", "Vienna", "Kettenbrckengasse 18", "1050", "30.02.2019", "thai", "+43(1)5812308", "www.website.at"));
+locations.push(new Restaurant("resources/img/lemonleaf.png", "Lemon Leaf", "Vienna", "Kettenbrckengasse 18", "1050", new Date(2018, 11, 25), "thai", "+43(1)5812308", "www.website.at"));
+locations.push(new Restaurant("resources/img/sixta.png", "Sixta", "Vienna", "Kettenbrckengasse 18", "1050", new Date(2018, 11, 14), "thai", "+43(1)5812308", "www.website.at"));
+locations.push(new Restaurant("resources/img/lemonleaf.png", "Lemon Leaf", "Vienna", "Kettenbrckengasse 18", "1050", new Date(2018, 11, 25), "thai", "+43(1)5812308", "www.website.at"));
+locations.push(new Restaurant("resources/img/sixta.png", "Sixta", "Vienna", "Kettenbrckengasse 18", "1050", new Date(2018, 11, 14), "thai", "+43(1)5812308", "www.website.at"));
 //events
+console.log(locations);
 for (var iter in locations) {
+    console.log(locations[iter]);
     locations[iter].display();
 }
--- resources/js/script.ts
+++ resources/js/script.ts
@@ -1,12 +1,16 @@
+// Set Content function
+
+// document.addEventListener("DOMContentLoaded")
+
 // Parent Class
 
 class Place {
-	image = "";
-	name = "";
-	city = "";
-	address = ""
-	zipCode = "";
-	date = "";
+	image: string = "";
+	name: string = "";
+	city: string = "";
+	address: string = ""
+	zipCode: string = "";
+	date: Date = new Date();
 	
 	constructor(image, name, city, address, zipCode, date) {
 		this.image = image;
@@ -17,28 +21,34 @@ class Place {
 		this.date = date;
 	}
 
-	// render function
-	display() {
-		return document.getElementById("places").innerHTML += 
-				`<div class="col-lg-3 col-md-6 col-sd-12">
+	//get card function
+	getCard(): string {
+		return `<div class="col-lg-3 col-md-6 col-sm-12">
+				
 				<div class="card">
   				<img class="card-img-top" src=${this.image} alt="location">
   				<div class="card-body">
     			<h5 class="card-title">${this.name}</h5>
-    			<p class="card-text"> ${this.address} ${this.zipCode} ${this.city}</p>
-    			<p>${this.date}</p>
+    			<p class="card-text">${this.address} ${this.zipCode}</p>
+    			<p class="card-text">${this.city}</p>
+    			<p>${this.date.toDateString()}</p>
   				</div>
 				</div>
 				</div>`;
+	}
+
+	// render function
+	display(): void {
+		document.getElementById("places").innerHTML += this.getCard();
 	}
 }
 
 // Child Classes
 
 class Restaurant extends Place {
-	category;
-	telephone;
-	website;
+	category: string;
+	telephone: string;
+	website: string;
 	constructor(image, name, city, address, zipCode, date, category, telephone, website) {
 		super(image, name, city, address, zipCode, date);
 		this.category = category;
@@ -46,20 +56,35 @@ class Restaurant extends Place {
 		this.website = website;
 	}
 
-	// render function
-	display() {
-		return document.getElementById("restaurants").innerHTML +=
-		`${super.display()}
-		<p>${this.category}</p>
-		<p>${this.telephone}</p>
-		<p>${this.website}</p>`;
+	//get card function
+	getCard(): string {
+		return `<div class="col-lg-3 col-md-6 col-sm-12">
+		<div class="card">
+  				<img class="card-img-top" src=${this.image} alt="location">
+  				<div class="card-body">
+    			<h5 class="card-title">${this.name}</h5>
+    			<p class="card-text">${this.address} ${this.zipCode}</p>
+    			<p class="card-text">${this.city}</p>
+    			<p class="card-text">${this.category}</p>
+				<p class="card-text">${this.telephone}</p>
+				<p class="card-text">${this.website}</p>
+    			<p class="card-text">${this.date.toDateString()}</p>
+  				</div>
+				</div>
+				</div>`;
+	}
+
+	//render function
+	display(): void {
+		document.getElementById("restaurants").innerHTML += this.getCard();
+		
 	}
 }
 
-class Events extends Place {
-	eventDate;
-	eventTime;
-	ticketPrice;
+class Concert extends Place {
+	eventDate: string;
+	eventTime: string;
+	ticketPrice: string;
 	constructor(image, name, city, address, zipCode, date, eventDate, eventTime, ticketPrice) {
 		super(image, name, city, address, zipCode, date);
 		this.eventDate = eventDate;
@@ -67,30 +92,52 @@ class Events extends Place {
 		this.ticketPrice = ticketPrice;
 	}
 
+	//get card function
+	getCard(): string {
+		return `<div class="col-lg-3 col-md-6 col-sm-12">
+		<div class="card">
+  				<img class="card-img-top" src=${this.image} alt="location">
+  				<div class="card-body">
+    			<h5 class="card-title">${this.name}</h5>
+    			<p class="card-text">${this.address} ${this.zipCode}</p>
+    			<p class="card-text">${this.city}</p>
+    			<p class="card-text">${this.eventDate}, ${this.eventTime}</p>
+				<p class="card-text">${this.ticketPrice}</p>
+    			<p class="card-text">${this.date.toDateString()}</p>
+  				</div>
+				</div>
+				</div>`;
+	}
+
 	// render function
-	display() {
-		return document.getElementById("events").innerHTML +=
-		`${super.display()}
-		<p>${this.eventDate}, ${this.eventTime}</p>
-		<p>${this.ticketPrice}</p>`;
+	display(): void {
+		document.getElementById("events").innerHTML += this.getCard();
 	}
 }
 
 // Instances
 
 
-var locations = [];
+let locations: Array<Place> = [];
 // places
-locations.push(new Place("resources/img/karlsplatz.jpg", "Karlsplatz", "Vienna", "Karlsplatz 1", "1010", "26 Feb 2019"));
-locations.push(new Place("resources/img/tiergarten.jpg", "Tiergarten", "Vienna", "Maxingstraße 13b", "1130", "29 Feb 2019"));
-locations.push(new Place("resources/img/karlsplatz.jpg", "Karlsplatz", "Vienna", "Karlsplatz 1", "1010", "26 Feb 2019"));
-locations.push(new Place("resources/img/tiergarten.jpg", "Tiergarten", "Vienna", "Maxingstraße 13b", "1130", "29 Feb 2019"));
+locations.push(new Place("resources/img/cliffs.jpg", "Cliffs of Moher", "Linnascor", "County Clare", "037111", new Date(2018, 11, 24)));
+locations.push(new Place("resources/img/skansen.jpg", "Skansen", "Stockholm", "Djurgårdsslätten 51", "11521", new Date(2017, 11, 24)));
+locations.push(new Place("resources/img/highgate.jpg", "Highgate Cemetery", "London", "Swain's Lane", "N66PJ", new Date(2019, 11, 24)));
+locations.push(new Place("resources/img/holyrood.jpg", "Holyrood Abbey", "Edinburgh", "Canongate 1", "8DX", new Date(2018, 1, 24)));
 
 // restaurants
-locations.push(new Restaurant("resources/img/lemonleaf.png", "Lemon Leaf", "Vienna", "Kettenbrckengasse 18", "1050", "30.02.2019", "thai", "+43(1)5812308", "www.website.at"));
+locations.push(new Restaurant("resources/img/lemonleaf.png", "Lemon Leaf", "Vienna", "Kettenbrckengasse 18", "1050", new Date(2018, 11, 25), "thai", "+43(1)5812308", "www.website.at"));
+locations.push(new Restaurant("resources/img/sixta.png", "Sixta", "Vienna", "Kettenbrckengasse 18", "1050", new Date(2018, 11, 14), "thai", "+43(1)5812308", "www.website.at"));
+locations.push(new Restaurant("resources/img/lemonleaf.png", "Lemon Leaf", "Vienna", "Kettenbrckengasse 18", "1050", new Date(2018, 11, 25), "thai", "+43(1)5812308", "www.website.at"));
+locations.push(new Restaurant("resources/img/sixta.png", "Sixta", "Vienna", "Kettenbrckengasse 18", "1050", new Date(2018, 11, 14), "thai", "+43(1)5812308", "www.website.at"));
 
 //events
 
-for(var iter in locations) {
+
+
+
+console.log(locations);
+for(let iter in locations) {
+	console.log(locations[iter]);
 	locations[iter].display();
 }

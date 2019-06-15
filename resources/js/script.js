03fb91a9953f26ee7a5e86df22f2b64662926ad2
Cosmic0wl <valentinaspanetta@gmail.com>
2019-06-15 16:21:55 +0200

    indentation

--- resources/js/script.js
+++ resources/js/script.js
@@ -27,11 +27,11 @@ var Place = /** @class */ (function () {
         this.zipCode = zipCode;
         this.date = date;
     }
-    //get card function
+    // Returns content
     Place.prototype.getCard = function () {
         return "<div class=\"col-lg-3 col-md-6 col-sm-12 my-2\">\n\t\t\t\t<div class=\"card\">\n  \t\t\t\t<img class=\"card-img-top d-none d-sm-none d-md-block\" src=" + this.image + " alt=\"location\">\n  \t\t\t\t<div class=\"card-body\">\n    \t\t\t<h5 class=\"card-title\">" + this.name + "</h5>\n    \t\t\t<p class=\"card-text\"><i class=\"fa fa-map-marker darkpink-text\"></i> " + this.address + " " + this.zipCode + ", " + this.city + "</p>\n    \t\t\t<small>Created: " + this.date.toDateString() + "</small>\n  \t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t\t</div>";
     };
-    // render function
+    // Render function
     Place.prototype.display = function () {
         document.getElementById("places").innerHTML += this.getCard();
     };
@@ -47,11 +47,11 @@ var Restaurant = /** @class */ (function (_super) {
         _this.website = website;
         return _this;
     }
-    //get card function
+    // Returns content
     Restaurant.prototype.getCard = function () {
         return "<div class=\"col-lg-3 col-md-6 col-sm-12 my-2\">\n\t\t<div class=\"card\">\n  \t\t\t\t<img class=\"card-img-top d-none d-sm-none d-md-block\" src=" + this.image + " alt=\"location\">\n  \t\t\t\t<div class=\"card-body\">\n    \t\t\t<h5 class=\"card-title\">" + this.name + "</h5>\n    \t\t\t<p class=\"card-text\"><i class=\"fa fa-map-marker darkpink-text\"></i> " + this.address + " " + this.zipCode + ", " + this.city + "</p>\n    \t\t\t<p class=\"card-text\">" + this.category + "</p>\n\t\t\t\t<p class=\"card-text\">" + this.telephone + "</p>\n\t\t\t\t<p class=\"card-text\"><a href=\"" + this.website + "\" target=\"_blank\" class=\"card-link darkpink-text\">Go to Website</a></p>\n    \t\t\t<small>Created: " + this.date.toDateString() + "</small>\n  \t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t\t</div>";
     };
-    //render function
+    // Render function
     Restaurant.prototype.display = function () {
         document.getElementById("restaurants").innerHTML += this.getCard();
     };
@@ -67,11 +67,11 @@ var Concert = /** @class */ (function (_super) {
         _this.ticketPrice = ticketPrice;
         return _this;
     }
-    //get card function
+    // Returns content
     Concert.prototype.getCard = function () {
         return "<div class=\"col-lg-3 col-md-6 col-sm-12 my-2\">\n\t\t<div class=\"card\">\n  \t\t\t\t<img class=\"card-img-top d-none d-sm-none d-md-block\" src=" + this.image + " alt=\"location\">\n  \t\t\t\t<div class=\"card-body\">\n    \t\t\t<h5 class=\"card-title\">" + this.name + "</h5>\n    \t\t\t<p class=\"card-text\">" + this.venue + "</p>\n    \t\t\t<p class=\"card-text\"><i class=\"fa fa-map-marker darkpink-text\"></i> " + this.address + " " + this.zipCode + ", " + this.city + "</p>\n    \t\t\t<p class=\"card-text\">" + this.eventDate + ", " + this.eventTime + "</p>\n\t\t\t\t<p class=\"card-text\">" + this.ticketPrice + "\u20AC</p>\n    \t\t\t<small>Created: " + this.date.toDateString() + "</small>\n  \t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t\t</div>";
     };
-    // render function
+    // Render function
     Concert.prototype.display = function () {
         document.getElementById("events").innerHTML += this.getCard();
     };
@@ -79,33 +79,34 @@ var Concert = /** @class */ (function (_super) {
 }(Place));
 // Instances
 var locations = [];
-// places
+// Places
 locations.push(new Place("resources/img/cliffs.jpg", "Cliffs of Moher", "County Clare", "Linnascor", "037111", new Date(2018, 11, 24)));
 locations.push(new Place("resources/img/skeppsholmen.jpg", "Skeppsholmen Island", "Stockholm", "Skeppsholmen", "11149", new Date(2017, 11, 24)));
 locations.push(new Place("resources/img/highgate.jpg", "Highgate Cemetery", "London", "Swain's Lane", "N66PJ", new Date(2019, 7, 12)));
 locations.push(new Place("resources/img/holyrood.jpg", "Holyrood Abbey", "Edinburgh", "Canongate", "8DX", new Date(2018, 1, 24)));
-// restaurants
-locations.push(new Restaurant("resources/img/peggyporschen.jpg", "Peggy Porschen", "London", "116 Ebury St", "SW1W", new Date(2019, 3, 25), "Bakery", "+43(1)5812308", "https://www.peggyporschen.com"));
-locations.push(new Restaurant("resources/img/milkinsta.jpg", "Milk Train", "London", "12 Tavistock St", "WC2E", new Date(2019, 3, 4), "Ice Cream Parlor", "+43(1)5812308", "https://www.milktraincafe.com"));
-locations.push(new Restaurant("resources/img/bao.jpeg", "BAO Soho", "London", "53 Lexington St", "W1F", new Date(2017, 3, 25), "Taiwanese", "+43(1)5812308", "https://baolondon.com/"));
-locations.push(new Restaurant("resources/img/saint.jpg", "Saint Aymes", "London", "231 Ebury St", "SW1W", new Date(2017, 11, 14), "Coffee Shop", "+43(1)5812308", "https://www.instagram.com/saintaymes/?hl=en"));
-// events
+// Restaurants
+locations.push(new Restaurant("resources/img/peggyporschen.jpg", "Peggy Porschen", "London", "116 Ebury St", "SW1W", new Date(2019, 3, 25), "Bakery", " +44 20 7730 1316", "https://www.peggyporschen.com"));
+locations.push(new Restaurant("resources/img/milkinsta.jpg", "Milk Train", "London", "12 Tavistock St", "WC2E", new Date(2019, 3, 4), "Ice Cream Parlor", "+44 20 7670 1316", "https://www.milktraincafe.com"));
+locations.push(new Restaurant("resources/img/bao.jpeg", "BAO Soho", "London", "53 Lexington St", "W1F", new Date(2017, 3, 25), "Taiwanese", "+44 90 7870 5316", "https://baolondon.com/"));
+locations.push(new Restaurant("resources/img/saint.jpg", "Saint Aymes", "London", "231 Ebury St", "SW1W", new Date(2017, 11, 14), "Coffee Shop", "+44 20 7262 1185", "https://www.instagram.com/saintaymes/?hl=en"));
+// Events
 locations.push(new Concert("resources/img/hozier.jpg", "Hozier", "Vienna", "Lothringerstraße 20", "1030", new Date(2019, 3, 17), "Wiener Konzerthaus", "10 Sep 2019", "8pm", "from 50"));
 locations.push(new Concert("resources/img/mitski.jpg", "Mitski", "Vienna", "Augartenbrücke 1", "1010", new Date(2019, 8, 17), "Flex", "13 Aug 2019", "8pm", "from 30"));
 locations.push(new Concert("resources/img/weyes.jpg", "Weyes Blood", "Berlin", "Schlesisches Tor", "10977", new Date(2016, 3, 17), "Bi Nuu", "26 Oct 2019", "8pm", "from 30"));
 locations.push(new Concert("resources/img/florence.jpg", "Florence and the Machine", "London", "Hyde Park", "142ha", new Date(2017, 3, 17), "Hyde Park", "13 Jul 2019", "8pm", "from 50"));
-// Sorting function ascending
+// Sorts instances in ascending order in their category
 function sortAscending() {
     locations.sort(function (post1, post2) {
         return post1.date.getTime() - post2.date.getTime();
     });
 }
-// Sorting function descending
+// Sorts instances in descending order in their category
 function sortDescending() {
     locations.sort(function (post1, post2) {
         return post2.date.getTime() - post1.date.getTime();
     });
 }
+// Retrieves current window path and executes sortAscending or sortDescending based on the path
 var currentPath = window.location.pathname;
 var currentPage = currentPath.split("/").pop();
 if (currentPage == "index_asc.html") {
@@ -114,7 +115,7 @@ if (currentPage == "index_asc.html") {
 else if (currentPage == "index_desc.html") {
     sortDescending();
 }
-// renders in html
+// Executes display() on every instance of the array
 for (var iter in locations) {
     locations[iter].display();
 }
--- resources/js/script.ts
+++ resources/js/script.ts
@@ -17,7 +17,7 @@ class Place {
 		this.date = date;
 	}
 
-	//get card function
+	// Returns content
 	getCard(): string {
 		return `<div class="col-lg-3 col-md-6 col-sm-12 my-2">
 				<div class="card">
@@ -31,7 +31,7 @@ class Place {
 				</div>`;
 	}
 
-	// render function
+	// Render function
 	display(): void {
 		document.getElementById("places").innerHTML += this.getCard();
 	}
@@ -50,7 +50,7 @@ class Restaurant extends Place {
 		this.website = website;
 	}
 
-	//get card function
+	// Returns content
 	getCard(): string {
 		return `<div class="col-lg-3 col-md-6 col-sm-12 my-2">
 		<div class="card">
@@ -67,10 +67,9 @@ class Restaurant extends Place {
 				</div>`;
 	}
 
-	//render function
+	// Render function
 	display(): void {
-		document.getElementById("restaurants").innerHTML += this.getCard();
-		
+		document.getElementById("restaurants").innerHTML += this.getCard();	
 	}
 }
 
@@ -87,7 +86,7 @@ class Concert extends Place {
 		this.ticketPrice = ticketPrice;
 	}
 
-	//get card function
+	// Returns content
 	getCard(): string {
 		return `<div class="col-lg-3 col-md-6 col-sm-12 my-2">
 		<div class="card">
@@ -104,7 +103,7 @@ class Concert extends Place {
 				</div>`;
 	}
 
-	// render function
+	// Render function
 	display(): void {
 		document.getElementById("events").innerHTML += this.getCard();
 	}
@@ -114,38 +113,40 @@ class Concert extends Place {
 
 let locations: Array<Place> = [];
 
-// places
+// Places
 locations.push(new Place("resources/img/cliffs.jpg", "Cliffs of Moher", "County Clare", "Linnascor", "037111", new Date(2018, 11, 24)));
 locations.push(new Place("resources/img/skeppsholmen.jpg", "Skeppsholmen Island", "Stockholm", "Skeppsholmen", "11149", new Date(2017, 11, 24)));
 locations.push(new Place("resources/img/highgate.jpg", "Highgate Cemetery", "London", "Swain's Lane", "N66PJ", new Date(2019, 7, 12)));
 locations.push(new Place("resources/img/holyrood.jpg", "Holyrood Abbey", "Edinburgh", "Canongate", "8DX", new Date(2018, 1, 24)));
 
-// restaurants
-locations.push(new Restaurant("resources/img/peggyporschen.jpg", "Peggy Porschen", "London", "116 Ebury St", "SW1W", new Date(2019, 3, 25), "Bakery", "+43(1)5812308", "https://www.peggyporschen.com"));
-locations.push(new Restaurant("resources/img/milkinsta.jpg", "Milk Train", "London", "12 Tavistock St", "WC2E", new Date(2019, 3, 4), "Ice Cream Parlor", "+43(1)5812308", "https://www.milktraincafe.com"));
-locations.push(new Restaurant("resources/img/bao.jpeg", "BAO Soho", "London", "53 Lexington St", "W1F", new Date(2017, 3, 25), "Taiwanese", "+43(1)5812308", "https://baolondon.com/"));
-locations.push(new Restaurant("resources/img/saint.jpg", "Saint Aymes", "London", "231 Ebury St", "SW1W", new Date(2017, 11, 14), "Coffee Shop", "+43(1)5812308", "https://www.instagram.com/saintaymes/?hl=en"));
+// Restaurants
+locations.push(new Restaurant("resources/img/peggyporschen.jpg", "Peggy Porschen", "London", "116 Ebury St", "SW1W", new Date(2019, 3, 25), "Bakery", " +44 20 7730 1316", "https://www.peggyporschen.com"));
+locations.push(new Restaurant("resources/img/milkinsta.jpg", "Milk Train", "London", "12 Tavistock St", "WC2E", new Date(2019, 3, 4), "Ice Cream Parlor", "+44 20 7670 1316", "https://www.milktraincafe.com"));
+locations.push(new Restaurant("resources/img/bao.jpeg", "BAO Soho", "London", "53 Lexington St", "W1F", new Date(2017, 3, 25), "Taiwanese", "+44 90 7870 5316", "https://baolondon.com/"));
+locations.push(new Restaurant("resources/img/saint.jpg", "Saint Aymes", "London", "231 Ebury St", "SW1W", new Date(2017, 11, 14), "Coffee Shop", "+44 20 7262 1185", "https://www.instagram.com/saintaymes/?hl=en"));
 
-// events
+// Events
 locations.push(new Concert("resources/img/hozier.jpg", "Hozier", "Vienna", "Lothringerstraße 20", "1030", new Date(2019, 3, 17), "Wiener Konzerthaus", "10 Sep 2019", "8pm", "from 50"));
 locations.push(new Concert("resources/img/mitski.jpg", "Mitski", "Vienna", "Augartenbrücke 1", "1010", new Date(2019, 8, 17), "Flex", "13 Aug 2019", "8pm", "from 30"));
 locations.push(new Concert("resources/img/weyes.jpg", "Weyes Blood", "Berlin", "Schlesisches Tor", "10977", new Date(2016, 3, 17), "Bi Nuu", "26 Oct 2019", "8pm", "from 30"));
 locations.push(new Concert("resources/img/florence.jpg", "Florence and the Machine", "London", "Hyde Park", "142ha", new Date(2017, 3, 17), "Hyde Park", "13 Jul 2019", "8pm", "from 50"));
 
 
-// Sorting function ascending
+// Sorts instances in ascending order in their category
 function sortAscending() {
     locations.sort(function(post1: Place, post2: Place) {
         return post1.date.getTime() - post2.date.getTime();
     });
 }
 
-// Sorting function descending
+// Sorts instances in descending order in their category
 function sortDescending() {
 	locations.sort(function(post1: Place, post2: Place) {
 		return post2.date.getTime() - post1.date.getTime();
 	});
 }
+
+// Retrieves current window path and executes sortAscending or sortDescending based on the path
 
 var currentPath = window.location.pathname;
 var currentPage = currentPath.split("/").pop();
@@ -156,7 +157,7 @@ if (currentPage == "index_asc.html") {
 	sortDescending();
 }
 
-// renders in html
+// Executes display() on every instance of the array
 
 for(let iter in locations) {
 	locations[iter].display();
